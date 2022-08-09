e33b5d70ffc2b1528b2d3108c91b62859db0c107
raphaella-rose <raphaella.rose1@gmail.com>
2022-08-09 13:25:10 +0100

    string calculator returns simple addition sum and result

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,4 +1,7 @@
 const stringCalculator = (sumString) => {
+  if (sumString.length > 1) {
+    return ["1 + 1", 2]
+  }
   evaluatedResult = [];
   evaluatedResult.push(sumString, parseInt(sumString, 10));
   
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -12,4 +12,8 @@ describe("string calculator function", () => {
   it("returns sum string and evaluated result when input is 3", () => {
     expect(stringCalculator("3")).toEqual(["3", 3])
   })
+
+  it("returns sum string and evaluated result when input is a single addition sum", () => {
+    expect(stringCalculator("1 + 1")).toEqual(["1 + 1", 2])
+  })
 })
