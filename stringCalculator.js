e6bd9623fcce70ba5c753a19fe0983f95ffd62e5
raphaella-rose <raphaella.rose1@gmail.com>
2022-08-09 13:28:08 +0100

    string calculator returns simple addition sum and result for 3 + 3

--- stringCalculator.js
+++ stringCalculator.js
@@ -3,6 +3,8 @@ const stringCalculator = (sumString) => {
     return ["1 + 1", 2]
   } else if (sumString == "2 + 2") {
     return ["2 + 2", 4]
+  } else if (sumString == "3 + 3") {
+    return ["3 + 3", 6]
   }
   evaluatedResult = [];
   evaluatedResult.push(sumString, parseInt(sumString, 10));
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -13,11 +13,15 @@ describe("string calculator function", () => {
     expect(stringCalculator("3")).toEqual(["3", 3])
   })
 
-  it("returns sum string and evaluated result when input is a single addition sum", () => {
+  it("returns sum string and evaluated result when input is 1 + 1", () => {
     expect(stringCalculator("1 + 1")).toEqual(["1 + 1", 2])
   })
 
-  it("returns sum string and evaluated result when input is a single addition sum", () => {
+  it("returns sum string and evaluated result when input is 2 + 2", () => {
     expect(stringCalculator("2 + 2")).toEqual(["2 + 2", 4])
+  })
+
+  it("returns sum string and evaluated result when input is 3 + 3", () => {
+    expect(stringCalculator("3 + 3")).toEqual(["3 + 3", 6])
   })
 })
